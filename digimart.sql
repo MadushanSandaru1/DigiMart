ee6681d73d4fe22240a39469eab69f0e302a7fda
MadushanSandaru1 <47908590+MadushanSandaru1@users.noreply.github.com>
2020-04-09 20:16:00 +0530

    Update digimart.sql

--- digimart.sql
+++ digimart.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 07, 2020 at 07:11 PM
+-- Generation Time: Apr 09, 2020 at 02:44 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -260,21 +260,9 @@ CREATE TABLE IF NOT EXISTS `customize_order` (
 --
 
 INSERT INTO `customize_order` (`id`, `customer_id`, `unit_price`, `quantity`, `date_time`, `is_posted`, `is_received`, `is_canceled`, `is_deleted`) VALUES
-(10, '', '169000.00', 0, '2020-02-25 01:57:43', 1, 1, 0, 0),
-(9, '', '158000.00', 0, '2020-03-27 01:55:56', 1, 1, 0, 0),
-(8, '', '11500.00', 0, '2020-03-24 21:07:46', 1, 1, 0, 0),
-(7, '', '11500.00', 0, '2020-03-24 19:35:55', 0, 1, 0, 0),
-(6, '', '158000.00', 0, '2020-03-24 19:35:55', 1, 1, 0, 0),
-(11, '', '135600.00', 0, '2020-03-25 02:03:43', 0, 0, 0, 0),
-(12, '', '11500.00', 0, '2020-03-25 02:03:43', 0, 0, 1, 0),
-(13, '', '412589.00', 0, '2020-03-24 19:35:50', 1, 1, 0, 0),
-(14, '', '11500.00', 0, '2020-03-24 19:35:55', 0, 0, 0, 0),
-(15, '', '135600.00', 0, '2020-03-25 02:03:43', 0, 0, 0, 0),
-(16, '', '135600.00', 0, '2020-03-25 02:03:43', 0, 0, 0, 0),
-(17, '', '676000.00', 0, '2020-04-05 15:36:51', 0, 0, 0, 0),
-(18, '', '1106000.00', 0, '2020-04-05 15:50:05', 0, 0, 0, 0),
-(19, '', '135600.00', 0, '2020-04-05 15:57:06', 0, 0, 1, 2),
-(20, '', '135600.00', 0, '2020-04-05 15:59:37', 0, 0, 1, 1);
+(3, 'C00001', '582000.00', 10, '2020-04-09 16:03:47', 0, 0, 1, 1),
+(2, 'C00001', '582000.00', 5, '2020-04-07 15:18:42', 0, 0, 0, 0),
+(1, 'C00001', '532000.00', 5, '2020-04-08 00:00:00', 1, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -289,28 +277,40 @@ CREATE TABLE IF NOT EXISTS `customize_order_product` (
   `product_id` int(11) NOT NULL,
   `is_deleted` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=48 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `customize_order_product`
 --
 
 INSERT INTO `customize_order_product` (`id`, `customize_order_id`, `product_id`, `is_deleted`) VALUES
-(10, 1, 2, 0),
-(9, 1, 5, 0),
-(8, 1, 1, 0),
-(7, 1, 1, 0),
-(6, 1, 5, 0),
-(11, 1, 6, 0),
-(12, 1, 1, 0),
-(13, 1, 7, 0),
-(14, 1, 1, 0),
-(15, 1, 6, 0),
-(16, 1, 6, 0),
-(17, 1, 2, 0),
-(18, 1, 5, 0),
-(19, 1, 6, 2),
-(20, 1, 6, 1);
+(35, 2, 17, 0),
+(34, 2, 15, 0),
+(33, 2, 15, 0),
+(32, 2, 14, 0),
+(31, 2, 12, 0),
+(30, 2, 8, 0),
+(29, 1, 19, 0),
+(28, 1, 20, 0),
+(27, 1, 18, 0),
+(26, 1, 17, 0),
+(25, 1, 15, 0),
+(24, 1, 16, 0),
+(23, 1, 14, 0),
+(22, 1, 12, 0),
+(21, 1, 10, 0),
+(36, 2, 18, 0),
+(37, 2, 19, 0),
+(38, 2, 19, 0),
+(39, 3, 8, 1),
+(40, 3, 12, 1),
+(41, 3, 14, 1),
+(42, 3, 15, 1),
+(43, 3, 15, 1),
+(44, 3, 17, 1),
+(45, 3, 18, 1),
+(46, 3, 19, 1),
+(47, 3, 19, 1);
 
 -- --------------------------------------------------------
 
@@ -354,7 +354,7 @@ CREATE TABLE IF NOT EXISTS `order_product` (
   `is_canceled` tinyint(1) NOT NULL DEFAULT 0,
   `is_deleted` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=39 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `order_product`
@@ -375,7 +375,25 @@ INSERT INTO `order_product` (`id`, `customer_id`, `product_id`, `quantity`, `uni
 (17, 'C00001', 2, 4, '676000.00', '2020-04-05 15:36:51', 0, 0, 0, 0),
 (18, 'C00001', 5, 7, '1106000.00', '2020-04-05 15:50:05', 0, 0, 0, 0),
 (19, 'C00001', 6, 9, '135600.00', '2020-04-05 15:57:06', 0, 0, 1, 2),
-(20, 'C00001', 6, 12, '135600.00', '2020-04-05 15:59:37', 0, 0, 1, 1);
+(20, 'C00001', 6, 12, '135600.00', '2020-04-05 15:59:37', 0, 0, 1, 1),
+(21, 'C00001', 10, 1, '10000.00', '2020-04-09 14:23:23', 0, 0, 0, 0),
+(22, 'C00001', 12, 1, '49500.00', '2020-04-09 14:23:23', 0, 0, 0, 0),
+(23, 'C00001', 14, 1, '155000.00', '2020-04-09 14:23:23', 0, 0, 0, 0),
+(24, 'C00001', 16, 1, '19500.00', '2020-04-09 14:23:23', 0, 0, 0, 0),
+(25, 'C00001', 16, 1, '19500.00', '2020-04-09 14:23:23', 0, 0, 0, 0),
+(26, 'C00001', 17, 1, '189500.00', '2020-04-09 14:23:23', 0, 0, 0, 0),
+(27, 'C00001', 18, 1, '3000.00', '2020-04-09 14:23:23', 0, 0, 0, 0),
+(28, 'C00001', 20, 1, '26500.00', '2020-04-09 14:23:23', 0, 0, 0, 0),
+(29, 'C00001', 19, 1, '59500.00', '2020-04-09 14:23:23', 0, 0, 0, 0),
+(30, 'C00001', 10, 1, '10000.00', '2020-04-09 14:25:00', 0, 0, 0, 0),
+(31, 'C00001', 12, 1, '49500.00', '2020-04-09 14:25:00', 0, 0, 0, 0),
+(32, 'C00001', 14, 1, '155000.00', '2020-04-09 14:25:00', 0, 0, 0, 0),
+(33, 'C00001', 16, 1, '19500.00', '2020-04-09 14:25:00', 0, 0, 0, 0),
+(34, 'C00001', 15, 1, '19500.00', '2020-04-09 14:25:00', 0, 0, 0, 0),
+(35, 'C00001', 17, 1, '189500.00', '2020-04-09 14:25:00', 0, 0, 0, 0),
+(36, 'C00001', 18, 1, '3000.00', '2020-04-09 14:25:00', 0, 0, 0, 0),
+(37, 'C00001', 20, 1, '26500.00', '2020-04-09 14:25:00', 0, 0, 0, 0),
+(38, 'C00001', 19, 1, '59500.00', '2020-04-09 14:25:00', 0, 0, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -394,7 +412,7 @@ CREATE TABLE IF NOT EXISTS `product` (
   `category_id` int(11) NOT NULL,
   `is_deleted` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product`
@@ -409,7 +427,18 @@ INSERT INTO `product` (`id`, `name`, `description`, `image`, `price`, `brand_id`
 (6, 'NANO X VIDEO LITE', 'Intel® Core™ i5-9400F (9M Cache, up to 4.10 GHz)\r\nMSI B365M PRO VDH\r\n16GB Gaming Memory 2666Mhz\r\n2TB Hard Disk 5400Rpm\r\n240GB SSSD ( Sequential Read of up to 500 MB/s)\r\nNvidia GTX 1650 4GB DDR5\r\nCooler Master Q500L Case\r\nInbuilt Wifi', '6.png', '135600.00', 11, 2, 0),
 (7, 'NANO X VIDEO PRO', 'AMD Ryzen™ 7 2700X (up to 4.3Ghz 8-cores 16-threads) 20M Cache X470 Motherboard RTX 2060 6GB DDR6 Graphics Card 32GB 3200Mhz Gaming Ram 4TB WD Black 7200RPM Samsung 970 EVO PLUS 500GB 650W Gold Certified FULLY MODULAR PSU Cooler Master MB500 Case 240 Liquid Cooler', '7.png', '400000.00', 11, 2, 0),
 (8, 'COOLER MASTER MASTERCASE H500P RGB', 'Materials Outlook: Plastic,<br>\r\nBody: Steel<br>\r\nSide panel: Tempered Glass, Steel<br>\r\nDimensions (LxWxH) 544 x 242 x 542mm / 21.4 x 9.5 x 21.3 inch<br>\r\nMotherboard Support Mini-ITX, Micro-ATX<br>\r\nATX, E-ATX (support upto 12\" x 10.7\")<br>\r\nExpansion Slots 7 + 2 (Support vertical graphics card installation)<br>\r\nDrive Bays 5.25\" 0<br>\r\n2.5\" / 3.5\" 2<br>\r\n2.5\" SSD 2 (Drive Bay support up to 5)<br>\r\nI/O Port USB 3.0 x 2<br>\r\nUSB 2.0 x 2<br>', 'casing1.png', '27000.00', 4, 11, 0),
-(9, 'ASUS PRIME A320M-E', 'AMD AM4 Socket for 2nd/1st AMD Ryzenâ„¢/2nd and 1st Gen AMD Ryzenâ„¢ with Radeonâ„¢ Vega Graphics/Athlonâ„¢ with Radeonâ„¢ Vega Graphics/7th Generation A-series/Athlon X4 Processors', 'motherboard1.png', '13900.00', 4, 6, 0);
+(9, 'ASUS PRIME A320M-E', 'AMD AM4 Socket for 2nd/1st AMD Ryzenâ„¢/2nd and 1st Gen AMD Ryzenâ„¢ with Radeonâ„¢ Vega Graphics/Athlonâ„¢ with Radeonâ„¢ Vega Graphics/7th Generation A-series/Athlon X4 Processors', 'motherboard1.png', '13900.00', 4, 6, 0),
+(10, 'CORSAIR 110R TEMPERED GLASS', 'Case Dimensions 418mm x 210mm x 480mm<br>\r\nMaximum GPU Length 330mm<br>\r\nMaximum PSU Length 180mm<br>\r\nMaximum CPU Cooler Height 160mm<br>\r\nCase Expansion Slots 7<br>\r\nCase Drive Bays â€(x2) 3.5in (x2) 2.5in\"<br>', 'casing2.png', '10000.00', 4, 11, 0),
+(11, 'ANTEC GX202', 'Dimensions 450 x 205 x 435 mm (DWH)<br>\r\nForm Factor Mid Tower<br>\r\nMaterials SPCC & Plastic<br>\r\nMainboard Support ATX / Micro-ATX / ITX<br>', 'casing3.png', '7000.00', 4, 11, 0),
+(12, 'ASUS ROG STRIX Z390-E GAMING', 'IntelÂ® Socket 1151 9th / 8th Gen IntelÂ® Coreâ„¢, PentiumÂ® Gold and CeleronÂ® Processors<br>\r\nSupports IntelÂ® 14 nm CPU<br>\r\nSupports IntelÂ® Turbo Boost Technology 2.0<br>\r\nThe IntelÂ® Turbo Boost Technology 2.0 support depends on the CPU types.', 'mb2.png', '49500.00', 4, 6, 0),
+(13, 'INTEL CORE I9-9900K', 'CPU Socket Type LGA 1151 (300 Series)<br>\r\nCore Name Coffee Lake<br>\r\n# of Cores 8-Core<br>\r\n# of Threads 16<br>\r\nOperating Frequency 3.6 GHz<br>\r\nMax Turbo Frequency 5.0 GHz<br>', 'pro1.png', '106000.00', 9, 5, 0),
+(14, 'AMD RYZEN 3950X (UP TO 4.7GHZ 16-CORES 32-THREADS) 72M CACHE', '# of CPU Cores 16<br>\r\n# of Threads 32<br>\r\nBase Clock 3.5GHz<br>\r\nMax Boost Clock Up to 4.7GHz<br>\r\nTotal L1 Cache 1MB<br>', 'pro2.png', '155000.00', 2, 5, 0),
+(15, 'KINGSTON HYPERX FURY 16GB DDR4 3200MHZ', 'Part Number: HX432C16FB3/16<br>\r\nSpecs: DDR4, 3200MHz, CL16, 1.35V, Unbuffered<br>\r\nTimings: 3200MHz, 16-18-18, 1.35V; 3000MHz, 15-17-17, 1.35V', 'ram1.png', '19500.00', 5, 7, 0),
+(16, 'CORSAIR VENGEANCE LPX 16GB (2 x 8GB) DDR4 DRAM 3600MHz Kit with Airflow Fan', 'Part Number: HX432C16FB3/16<br>\r\nSpecs: DDR4, 3200MHz, CL16, 1.35V, Unbuffered<br>\r\nTimings: 3200MHz, 16-18-18, 1.35V; 3000MHz, 15-17-17, 1.35V', 'ram2.png', '19500.00', 5, 7, 0),
+(17, 'ASUS STRIX RTX 2080 SUPER 8GB DDR6', 'NVIDIAÂ® GeForceÂ® RTX 2080 SUPER<br>\r\nBus Standard PCI Express 3.0<br>\r\nOpenGL OpenGLÂ®4.6<br>\r\nVideo Memory GDDR6 8GB', 'gc1.png', '189500.00', 4, 8, 0),
+(18, 'ASUS DVDRW 24X', '1 year warranty', 'od1.png', '3000.00', 4, 12, 0),
+(19, 'SAMSUNG 970 EVO PLUS M.2 NVME 1TB', 'Form Factor M.2 2280<br>\r\nCapacity 1TB<br>\r\nMemory Components V-NAND 3-bit MLC', 'st1.png', '59500.00', 13, 10, 0),
+(20, 'SAMSUNG 970 EVO PLUS M.2 NVME 500GB', 'Form Factor M.2 2280<br>\r\nCapacity 500GB<br>\r\nMemory Components V-NAND 3-bit MLC', 'st2.png', '26500.00', 13, 10, 0);
 
 -- --------------------------------------------------------
 
@@ -450,13 +479,6 @@ CREATE TABLE IF NOT EXISTS `quotation` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=47 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `quotation`
---
-
-INSERT INTO `quotation` (`id`, `customer_id`, `product_id`, `date_time`) VALUES
-(1, 'C00001', 1, '2020-03-24 16:03:20');
-
 -- --------------------------------------------------------
 
 --
@@ -471,13 +493,6 @@ CREATE TABLE IF NOT EXISTS `shopping_cart` (
   `date_time` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `shopping_cart`
---
-
-INSERT INTO `shopping_cart` (`id`, `customer_id`, `product_id`, `date_time`) VALUES
-(1, 'C00001', 5, '2020-03-25 02:07:04');
 
 -- --------------------------------------------------------
 
